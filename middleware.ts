7affb34a18712c8b266830ebc7373d762467d3f7
Tharun4743 <tharunkumark42007@gmail.com>
2026-06-28 23:17:27 +0530

    chore: push all recent changes

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { authConfig } from "./auth.config"
 
 const { auth } = NextAuth(authConfig)
 
-export default auth((req) => {
+export default auth((req: any) => {
   const isLoggedIn = !!req.auth
   const { nextUrl } = req
 
